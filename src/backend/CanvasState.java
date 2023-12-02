e890200114716d936d643f8bc6bd078e6fdc1789
Ana Negre <98236062+anita476@users.noreply.github.com>
2023-12-02 11:59:11 -0300

    CanvasState ahora extiende a arraylist + cambios de consistencia al respecto

--- src/backend/CanvasState.java
+++ src/backend/CanvasState.java
@@ -2,22 +2,8 @@ package backend;
 
 import backend.model.*;
 import java.util.ArrayList;
-import java.util.List;
 
-public class CanvasState {
+public class CanvasState extends ArrayList<Figure>{
 
-    private final List<Figure> list = new ArrayList<>();
-
-    public void addFigure(Figure figure) {
-        list.add(figure);
-    }
-
-    public void deleteFigure(Figure figure) {
-        list.remove(figure);
-    }
-
-    public Iterable<Figure> figures() {
-        return new ArrayList<>(list);
-    }
 
 }
