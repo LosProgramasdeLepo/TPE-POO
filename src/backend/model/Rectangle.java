4655c4cb6b60282df80c8fdcdbf61a75369c6b3e
Ana Negre <98236062+anita476@users.noreply.github.com>
2023-12-05 00:36:12 -0300

    scaleUp y scaleDown implementadas para rectangulos y círculos

--- src/backend/model/Rectangle.java
+++ src/backend/model/Rectangle.java
@@ -106,7 +106,18 @@ public class Rectangle extends Figure {
 
     @Override
     public void rotateRight() {
-
+        System.out.println("%s, %s".formatted(topLeft,bottomRight));
+        double deltaX = (getCenter().getX() - getTopLeft().getX())/2;
+        double deltaY = (getCenter().getY() - getTopLeft().getY())/2;
+        if(getHeight()<=getWidth()) {
+            this.topLeft.move(deltaX, deltaY);
+            this.bottomRight.move(-deltaX,-deltaY);
+            System.out.println("%s,%s".formatted(topLeft,bottomRight));
+        }
+        else{
+            this.topLeft.move(-deltaX, - deltaY);
+            this.bottomRight.move(deltaX, deltaY);
+        }
     }
 
     @Override
@@ -141,12 +152,18 @@ public class Rectangle extends Figure {
 
     @Override
     public void scaleUp() {
-
+        scale(0.25);
     }
-
+//todo quizas hacer en figure una constante para los 0.25
     @Override
     public void scaleDown() {
-
+        scale(-0.25);
+    }
+    private void scale(double percent){
+        double deltaX = (percent/2)*getWidth();
+        double deltaY = (percent/2)* getHeight();
+        topLeft.move(-deltaX, -deltaY);
+        bottomRight.move(deltaX,deltaY);
     }
 
 }
